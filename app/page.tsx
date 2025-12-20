07e96df75a45a2de394d9dd07de53054228574b7
OrcDev <urosmiric88@gmail.com>
2025-12-21 00:08:30 +0100

    style(page) enhance loading skeleton layout for improved user experience

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,23 @@ export default async function Page() {
 
       <Suspense
         fallback={
-          <div className="flex flex-wrap gap-2">
-            <Skeleton className="h-10 w-20" />
-            <Skeleton className="h-10 w-20" />
-            <Skeleton className="h-10 w-20" />
-            <Skeleton className="h-10 w-20" />
-            <Skeleton className="h-10 w-20" />
-            <Skeleton className="h-10 w-20" />
+          <div className="flex flex-col gap-2 min-h-30">
+            <Skeleton className="h-2 w-20 mt-3" />
+            <div className="flex flex-wrap gap-2">
+              <Skeleton className="h-7 w-24" />
+              <Skeleton className="h-7 w-24" />
+              <Skeleton className="h-7 w-24" />
+              <Skeleton className="h-7 w-24" />
+              <Skeleton className="h-7 w-24" />
+            </div>
+
+            <div className="flex flex-wrap gap-2">
+              <Skeleton className="h-7 w-24" />
+              <Skeleton className="h-7 w-24" />
+              <Skeleton className="h-7 w-24" />
+              <Skeleton className="h-7 w-24" />
+              <Skeleton className="h-7 w-24" />
+            </div>
           </div>
         }
       >
--- components/latest-creatures.tsx
+++ components/latest-creatures.tsx
@@ -6,7 +6,7 @@ export default async function LatestCreatures() {
   const creatures = await getTenLatestCreatures();
 
   return (
-    <div className="flex flex-col w-full sm:max-w-lg">
+    <div className="flex flex-col w-full sm:max-w-lg min-h-30">
       <h2 className="text-sm">Latest Creatures</h2>
       <div className="flex flex-wrap gap-2">
         {creatures.map((creature) => (
