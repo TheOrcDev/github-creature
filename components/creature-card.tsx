dcc0b7aa0e1a5db9a282bd72b0d8ed89972fe517
OrcDev <urosmiric88@gmail.com>
2025-12-21 22:14:47 +0100

    feat(liquid-ether) add LiquidEther component for dynamic visual effects and integrate into CreatureCard for enhanced presentation

--- components/creature-card.tsx
+++ components/creature-card.tsx
@@ -2,15 +2,8 @@ import { getCreatureByGithubUsername } from "@/server/creatures";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import LiquidEther from "./liquid-ether";
 
 type CreatureCardProps = {
   params: Promise<{ username: string }>;
@@ -44,7 +37,26 @@ export default async function CreatureCard({ params }: CreatureCardProps) {
   }
 
   return (
-    <Card className="p-0 w-96">
+    <Card className="p-0 w-96 relative shadow-2xl shadow-purple-500/40">
+      <div className="w-full absolute top-0 left-0 h-full border-2 border-purple-500">
+        <LiquidEther
+          colors={["#5227FF", "#FF9FFC", "#B19EEF"]}
+          mouseForce={20}
+          cursorSize={100}
+          isViscous={false}
+          viscous={30}
+          iterationsViscous={32}
+          iterationsPoisson={32}
+          resolution={0.5}
+          isBounce={false}
+          autoDemo={true}
+          autoSpeed={0.5}
+          autoIntensity={2.2}
+          takeoverDuration={0.25}
+          autoResumeDelay={3000}
+          autoRampDuration={0.6}
+        />
+      </div>
       <CardHeader className="p-0 flex flex-col gap-3 items-center justify-center">
         <Image
           src={creature.image}
