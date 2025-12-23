3d4a720cd4d70964931a798afca8466ee53c9b41
OrcDev <urosmiric88@gmail.com>
2025-12-23 22:55:03 +0100

    refactor(header) reorganize logo placement within the header component for improved structure

--- components/header.tsx
+++ components/header.tsx
@@ -16,16 +16,16 @@ const navItems = [
 export default function Header() {
   return (
     <header className="absolute top-0 p-4 w-full flex justify-between">
-      <Link href="/">
-        <Image
-          src="/github-creature-logo.png"
-          alt="Logo"
-          width={50}
-          height={50}
-        />
-      </Link>
-
       <div className="flex items-center gap-5">
+        <Link href="/">
+          <Image
+            src="/github-creature-logo.png"
+            alt="Logo"
+            width={50}
+            height={50}
+          />
+        </Link>
+
         {navItems.map((item) => (
           <Link key={item.href} href={item.href}>
             {item.label}
