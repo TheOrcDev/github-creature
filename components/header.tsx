d0879b8ec66ee004aff39586c8048c5e1f2f11dd
OrcDev <urosmiric88@gmail.com>
2026-01-09 15:25:16 +0100

    feat(header): add StarsCount component to display GitHub stars and enhance button styling

--- components/header.tsx
+++ components/header.tsx
@@ -43,12 +43,33 @@ export default function Header() {
           rel="noopener noreferrer"
           href="https://github.com/theorcdev/github-creature"
         >
-          <Button variant="ghost" size="icon">
+          <Button
+            variant="ghost"
+            size="icon"
+            className="flex items-center gap-2 px-6"
+          >
             <HugeiconsIcon icon={Github} />
+            <StarsCount />
           </Button>
         </Link>
         <ModeSwitcher />
       </div>
     </header>
+  );
+}
+
+export async function StarsCount() {
+  "use cache";
+
+  const data = await fetch(
+    "https://api.github.com/repos/theorcdev/github-creature"
+  );
+  const json = await data.json();
+  const stars = json.stargazers_count;
+
+  return (
+    <span className="w-12 text-muted-foreground text-xs tabular-nums">
+      {stars >= 1000 ? `${(stars / 1000).toFixed(1)}k` : stars.toLocaleString()}
+    </span>
   );
 }
