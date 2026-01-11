18adb938285ec7d45385e4a36e64d27641104cf6
OrcDev <urosmiric88@gmail.com>
2026-01-12 00:43:25 +0100

    feat(app) add summon page

---
+++ app/summon/page.tsx
@@ -0,0 +1,40 @@
+import { Suspense } from "react";
+
+import { SubmitGithubForm } from "@/components/forms/github-form";
+import LatestCreatures from "@/components/latest-creatures";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default async function Page() {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen gap-5 px-2">
+      <Suspense>
+        <SubmitGithubForm />
+      </Suspense>
+
+      <Suspense
+        fallback={
+          <div className="flex flex-col gap-2 min-h-30">
+            <Skeleton className="h-2 w-20 mt-3" />
+            <div className="flex flex-wrap gap-2">
+              <Skeleton className="h-7 w-24" />
+              <Skeleton className="h-7 w-24" />
+              <Skeleton className="h-7 w-24" />
+              <Skeleton className="h-7 w-24" />
+              <Skeleton className="h-7 w-24" />
+            </div>
+
+            <div className="flex flex-wrap gap-2">
+              <Skeleton className="h-7 w-24" />
+              <Skeleton className="h-7 w-24" />
+              <Skeleton className="h-7 w-24" />
+              <Skeleton className="h-7 w-24" />
+              <Skeleton className="h-7 w-24" />
+            </div>
+          </div>
+        }
+      >
+        <LatestCreatures />
+      </Suspense>
+    </main>
+  );
+}
